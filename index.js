0689c07f353879493bb9981e162962ec0012428e
marko delgadillo <markoneseven@gmail.com>
2017-12-02 17:02:09 -0800

    #1 Function to render the teacher user interface created and materialize css is linked to the html.

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ const app = express()
 const seed = require('./seed.js')
 const path = require('path')
 app.use(express.static('./public'))
+
+MongoClient.connect('mongodb://localhost/library', function(err, db) {
+  if (err) {
+    console.log(err)
+    process.exit(1)
+  }
+  const students = db.collection('students')
+
+  app.get('/students', function(req, res) {})
+
+  app.post()
+
+  app.pull()
+
+  app.delete()
+
+  app.listen(7777, function() {
+    console.log('Listening on Port 7777.')
+  })
+})
